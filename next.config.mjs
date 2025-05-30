607247eb15d14b0df3a71712880c082c988bf407
Syifa Utami <syifa.tam.utami@gmail.com>
2025-05-31 02:33:40 +0700

    add all

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'm.media-amazon.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'api.lorem.space',
-        },
-        {
-          protocol: 'https',
-          hostname: 'placeimg.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'upload.wikimedia.org',
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn.dummyjson.com',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+
+export default nextConfig;
